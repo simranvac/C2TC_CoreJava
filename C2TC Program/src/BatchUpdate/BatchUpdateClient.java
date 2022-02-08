6e6f3a4cb3ce284441e82e036b6db0ca8bf858a0
amita <amita@DESKTOP-T0K7G15>
2022-02-09 02:21:35 +0530

    First commit

---
+++ C2TC Program/src/BatchUpdate/BatchUpdateClient.java
@@ -0,0 +1,41 @@
+package BatchUpdate;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import usingStatement.DBUtil1;
+
+public class BatchUpdateClient {
+
+	public static void main(String[] args) throws SQLExecption {
+		// TODO Auto-generated method stub
+		Connection connection=DBUtil.getConnection();
+		if(connection !=null)
+			 System.out.println("JDBC:connection is taken.");
+		    
+		   Statement st=connection.createStatement();
+		   
+		   String sql1="INSERT INTO employee_table(employee_id,employee_ame,email,esalary,bonus,doj)"+
+		             "VALUES (9,'Simran','simi2104@gmail.com',6000,3000,'2022-02-08')";
+		    st.addBatch(sql1);
+		    
+		    String sql2="update employee_Table set employee_name='Abhisekh' where employee_id=8";
+		    st.addBatch(sql2);
+		    
+		    String sql3="delete from employee_table where employee_id=5";
+		         st.addBatch(sql3);
+		         
+		         int i[]=st.executeBatch();
+		         for (int j=0;j<i.length;j++)
+		         {
+		        	 System.out.println(j);
+		        	   }
+		         
+		         try {
+		        	 connection.close();
+		        	 st.close();
+		         }catch(Execption e) {
+		        	 System.out.println
+		         }
+		         }
