5a0fabdcaab4904dbebb696b1adb3d5c291b55cc
amita <amita@DESKTOP-T0K7G15>
2022-02-12 01:18:18 +0530

    First commit

---
+++ C2TC Program/src/TreeMap.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+ class TreeMap {
+
+	public static void main(String[] args) {
+       java.util.TreeMap<Integer,String> map=new TreeMap<Integer,String>();
+       map.put(100, "Amit");
+       map.put(102, "Ravi");
+       map.put(101,"Vijay" );
+       map.put(103, "Rahul");
+       //map.put(null,"Xaviour");
+       map.put(105,null);
+       
+       for(Map.entry m:map.entrySet()){
+    	   System.out.println(m.getKey()+" "+ m.getValue());
+    	   // TODO Auto-generated method stub
+       }
+
+	}
+
+}
